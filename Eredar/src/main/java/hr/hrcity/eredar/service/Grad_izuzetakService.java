261c8f7d505cd1a4224302c46a909248dff5a0a8
abrickitw <antun.vedran.dajkovic@student.mev.hr>
2023-03-22 10:36:48 +0100

    commit

--- Eredar/src/main/java/hr/hrcity/eredar/service/Grad_izuzetakService.java
+++ Eredar/src/main/java/hr/hrcity/eredar/service/Grad_izuzetakService.java
@@ -3,7 +3,6 @@ package hr.hrcity.eredar.service;
 import hr.hrcity.eredar.model.Grad_izuzetak;
 
 public interface Grad_izuzetakService {
-
 	Grad_izuzetak createEntity(Grad_izuzetak data);
 
 	Grad_izuzetak updateEntity(Grad_izuzetak data);
--- Eredar/src/main/java/hr/hrcity/eredar/service/Grad_izuzetakServiceImpl.java
+++ Eredar/src/main/java/hr/hrcity/eredar/service/Grad_izuzetakServiceImpl.java
@@ -72,7 +72,5 @@ public class Grad_izuzetakServiceImpl implements Grad_izuzetakService {
 		}
 
 	}
-
-
 }
 
